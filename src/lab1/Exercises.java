4d019b083adc0fefe1b0a14243196d79be974895
Gabriela <gabrielamico@yahoo.com>
2021-11-01 20:55:15 +0200

    first exercises

---
+++ src/lab1/Exercises.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class Exercises {
+    public void arrays(){
+        char[] copyForm = { 'd', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a', 't', 'e', 'd'};
+        char[] copyTo = new char[7];
+
+        System.arraycopy(copyForm, 2, copyTo, 0, 7);
+        System.out.println(copyTo);
+
+
+    }
+}
--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,7 +1,9 @@
 package lab1;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
+        Exercises ex = new Exercises();
+        ex.arrays();
         System.out.println("Atelierul digital Google!");
     }
 
