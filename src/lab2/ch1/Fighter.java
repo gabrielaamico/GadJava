4f4a49797e701e2f62a3cfa54a0df781fd3a17d2
Gabriela <gabrielamico@yahoo.com>
2021-11-03 21:34:34 +0200

    lab 2 ch1

---
+++ src/lab2/ch1/Fighter.java
@@ -0,0 +1,25 @@
+package lab2.ch1;
+
+public class Fighter {
+    private String name;
+    private int health;
+    private int damagePerAttack;
+
+ public Fighter(String name, int health, int damagePerAttack){
+     this.name = name;
+     this.health = health;
+     this.damagePerAttack = damagePerAttack;
+ }
+
+ public void attack(Fighter f){
+     f.health=f.health - damagePerAttack;
+ }
+
+ public String getName(){
+     return name;
+ }
+
+ public int getHealth(){
+     return health;
+ }
+}
